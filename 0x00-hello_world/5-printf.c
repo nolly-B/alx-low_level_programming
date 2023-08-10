51e621a37649f08e508de7dadebdd0af43aac0c2
nolly-B <lebepebonolo7@gmail.com>
2023-08-10 13:11:57 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the exact text followed by a new line
+ *
+ * Return: always 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
