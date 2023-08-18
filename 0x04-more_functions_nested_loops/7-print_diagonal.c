0566bc69073a53655cb9c17a8471152fec03775d
nolly-B <lebepebonolo7@gmail.com>
2023-08-18 13:25:56 -0700

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,19 +7,18 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 		putchar('\n');
-	else
+	for (i = 0 ; i < n ; i++)
 	{
-		int i, j;
-
-		for (i = 0 ; i < n ; i++)
+		for (j = 0 ; j < i ; j++)
 		{
-			for (j = 0 ; j <= i ; j++)
-				putchar(' ');
-			putchar('\\');
-			putchar('\n');
+			putchar(' ');
 		}
+		putchar('\\');
+		putchar('\n');
 	}
 }
 
