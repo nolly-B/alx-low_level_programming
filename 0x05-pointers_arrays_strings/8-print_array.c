3e6a3704dfb2d73acb7a64cee6512ac9effd1d5f
nolly-B <lebepebonolo7@gmail.com>
2023-08-21 13:59:22 -0700

    .

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - main function
+ * @a: function parameter
+ * @n: function parameter
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
