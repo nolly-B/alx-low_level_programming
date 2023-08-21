e3cf6abba8e276d8f1c78cb2b7fb01f450620089
nolly-B <lebepebonolo7@gmail.com>
2023-08-21 13:18:26 -0700

    .

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - main fuction
+ * @str: function parameter
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		putchar(str[i]);
+	putchar(str[i]);
+}
