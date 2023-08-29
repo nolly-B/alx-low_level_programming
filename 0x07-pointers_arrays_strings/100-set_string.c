3f3f41db5db435ea55b4a58ec67656eb7e5674b2
nolly-B <lebepebonolo7@gmail.com>
2023-08-29 04:23:42 -0700

    .

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - main function
+ * @s: function parameter
+ * @to: function parameter
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
