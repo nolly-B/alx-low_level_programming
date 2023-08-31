317fc114ce10151615e0ccd2ea183a8dcc969cdd
nolly-B <lebepebonolo7@gmail.com>
2023-08-31 13:38:30 -0700

    .

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * factorial - main function
+ * @n: function parameter
+ * Return: void
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
